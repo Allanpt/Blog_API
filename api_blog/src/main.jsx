ac66f30f8c633653821c264320837d79e10e3724
Allanpt <jatallantavares@gmail.com>
2023-10-31 11:32:12 -0300

    att 2

--- api_blog/src/main.jsx
+++ api_blog/src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Home from './routes/Home.jsx'
-import NewPosts from './routes/NewPosts.jsx'
 
 
 import './index.css'
@@ -17,10 +16,6 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-      },
-      {
-        path: "/new",
-        element: <NewPosts />,
       },
     ]
   },
